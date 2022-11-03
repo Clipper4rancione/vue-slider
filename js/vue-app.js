79e713904d44596934d5d81946c969ba6ce45d8d
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-11-03 16:21:03 +0100

    [modified] images source

--- js/vue-app.js
+++ js/vue-app.js
@@ -6,11 +6,29 @@ createApp({
             country: {
                 images: [
                     
-                    'cyberpunk.jpg',
-                    'chile.jpg',
-                    'mountain-lake.jpg',
-                    'peru.jpg'
-                
+                    'img/cyberpunk.jpg',
+                    'img/chile.jpg',
+                    'img/mountain-lake.jpg',
+                    'img/peru.jpg',
+                    'img/argentina.jpg'
+                ],
+                name: [
+                    'Ciberpunk Oriental City',
+                    'Cile',
+                    'Colombia',
+                    'Perù',
+                    'Argentina'
+                ],
+                caption: [
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, consectetur.',
+                    
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+
+                    'Pablo\'s Home.',
+
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. amet consectetur adipisicing elit.',
+
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
                 ]
             }
         }
